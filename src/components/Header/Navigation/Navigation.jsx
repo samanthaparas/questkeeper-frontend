7885a91ee9342a70072f29c4c0682606ee8388e0
Samantha Paras <samantha.paras94@gmail.com>
2026-06-22 21:48:39 -0500

    Update navigation bar to include races, classes, backgrounds, and spells

--- src/components/Header/Navigation/Navigation.jsx
+++ src/components/Header/Navigation/Navigation.jsx
@@ -7,6 +7,23 @@ function Navigation() {
       <Link className="navigation__link" to="/">
         Home
       </Link>
+
+      <Link className="navigation__link" to="/races">
+        Races
+      </Link>
+
+      <Link className="navigation__link" to="/classes">
+        Classes
+      </Link>
+
+      <Link className="navigation__link" to="/backgrounds">
+        Backgrounds
+      </Link>
+
+      <Link className="navigation__link" to="/spells">
+        Spells
+      </Link>
+
       <Link className="navigation__link" to="/about">
         About
       </Link>
